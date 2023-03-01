a1abc0d2179a0df4b88fbc1603976d89bebfe537
KawaManmi <kawamam2007@gmail.com>
2023-03-01 22:32:50 +0000

    Some simple modification

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -4,10 +4,14 @@ import { useTheme } from "next-themes";
 const ServiceCard = ({ name, description }) => {
   const { theme } = useTheme();
   const [mounted, setMounted] = useState();
-
+  const [showAll, setShowAll] = useState(false);
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
   useEffect(() => {
     setMounted(true);
   }, []);
+  const shortenedText = showAll ? description : description.slice(0, 100);
   return (
     <div
       className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
@@ -16,10 +20,13 @@ const ServiceCard = ({ name, description }) => {
     >
       <h1 className="text-3xl">{name ? name : "Heading"}</h1>
       <p className="mt-5 opacity-40 text-xl">
-        {description
-          ? description
-          : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "}
+      {shortenedText}
       </p>
+      {description.length > 100 && (
+        <button onClick={toggleShowAll}>
+          {showAll ? "See Less" : "See More"}
+        </button>
+      )}
     </div>
   );
 };
